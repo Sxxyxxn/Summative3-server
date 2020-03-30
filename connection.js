aa6ec727694a535345abf4577df5ea2f0affaa75
leejisun <leejisun@leejisunui-MacBook-Pro.local>
2020-03-30 13:10:17 +1300

    connection

---
+++ connection.js
@@ -0,0 +1,5 @@
+let dbname = "autotrade";
+let username = "root";
+let password = "root";
+
+exports.atlas = `mongodb+srv://${username}:${password}@yoobee-gtgz5.gcp.mongodb.net/${dbname}?retryWrites=true&w=majority`;
